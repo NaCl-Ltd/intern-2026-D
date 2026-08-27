6241ddd298019257ce58ddf76f5feae3cd4eec21
yuma <yumatomo3@gmail.com>
2026-08-27 15:59:29 +0900

    zokuseinosiborikomi

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,12 @@ class StaticPagesController < ApplicationController
 
   def home
     if logged_in?
-      @micropost  = current_user.microposts.build
-      @feed_items = current_user.feed.paginate(page: params[:page])
+      @micropost = current_user.microposts.build
+      @feed_items = current_user.feed
+      if params[:category].present?
+        @feed_items = @feed_items.where(category: params[:category])
+      end
+      @feed_items = @feed_items.paginate(page: params[:page])
     end
   end
 
